71c48f4a82d26aaf4f3de8aef879f644ba2351ff
AlanAOliveira <alanalves123456789@gmail.com>
2026-03-26 15:58:19 -0300

    Show current datetime in app header
    Import datetime and display the current date/time in the Gradio app header by adding a formatted `now` variable and including it in the Markdown title. Also updates the Databricks sync snapshot timestamp for app.py.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import gradio as gr
 import pandas as pd
+import datetime
 
 data = pd.DataFrame({'x': [x for x in range(30)],
                      'y': [2 ** x for x in range(30)]})
+
+# Current date and time header
+now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 # Display the data with Gradio
 with gr.Blocks(css='footer {visibility: hidden}') as gradio_app:
     with gr.Row():
         with gr.Column(scale=3):
-            gr.Markdown('# Inventario YARD!')
+            gr.Markdown(f'# Inventario YARD!\n\n**{now}**')
             gr.ScatterPlot(value=data, height=400, width=700,
                            container=False, x='x', y='y',
                            y_title='Fun with data', x_title='Apps')
